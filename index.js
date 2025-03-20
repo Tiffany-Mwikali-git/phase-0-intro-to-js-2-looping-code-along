5896c98e972091c2859fd1cea3ff761a0e1a7a1e
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-03-20 11:34:33 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this fill
+function writeCards(names,event){
+    let messages= []
+    for(let i=0; i<names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+            return messages;
+  
+}
+
+ function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+    }
+}
+
+
+
